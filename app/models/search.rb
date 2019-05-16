8b3580441c88a76fff8498626f2bd5e0d309b77a
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2019-05-17 08:32:10 +0900

    Initial commit

---
+++ app/models/search.rb
@@ -0,0 +1,9 @@
+class Search
+  attr_accessor :title, :image_url, :url
+
+  def initialize(title, image_url, url)
+    @title = title
+    @image_url = image_url
+    @url = url
+  end
+end
---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,4 @@
+RakutenWebService.configure do |c|
+  c.application_id = '1098958799307606422'
+  c.affiliate_id = '165d1a29.8d94c3de.165d1a2a.674be0c1'
+end
